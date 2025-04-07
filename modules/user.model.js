a53a0d278483d30b7e71e76074ba7178ec0af6b1
24LP2767 24LP2767 <24LP2767@G301-CC0202.local>
2025-04-07 16:40:05 +0800

    ss

---
+++ modules/user.model.js
@@ -0,0 +1,6 @@
+const usersSchema = new ({
+    userName: {type: String, require: true},
+    email: {type: String, require: true },
+    password: {type: String, require: true},
+    // receivedDonation: {ref: }
+})
