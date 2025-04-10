172ff2b78318e92c0d9f211706e8f16cbd182ec5
24LP2767 24LP2767 <24LP2767@G301-CC0202.local>
2025-04-10 15:43:38 +0800

    ss

---
+++ back-end/src/controller/donation/delete-donation.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+export const deleteDonation = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+  } catch (error) {}
+};
