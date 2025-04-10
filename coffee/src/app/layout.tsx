f6e1fc75bf78cd3cf8809a98de5ee66a1bcc3acb
Vskroi <24LP1862@G301-CC0201.local>
2025-04-10 15:18:35 +0800

    a

--- coffee/src/app/layout.tsx
+++ coffee/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { CreateAccountProvider } from "@/providers/CreateAccountProvider";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,7 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <CreateAccountProvider>{children}</CreateAccountProvider>
+      </body>
     </html>
   );
 }
