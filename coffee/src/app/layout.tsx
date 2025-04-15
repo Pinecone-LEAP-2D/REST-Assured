76479a7aa0af2240a1fec53a217570e81c8a8435
24LP2767 24LP2767 <24LP2767@G301-CC0202.local>
2025-04-15 17:36:19 +0800

    style toast notif

--- coffee/src/app/layout.tsx
+++ coffee/src/app/layout.tsx
@@ -6,13 +6,8 @@ import { UpdateProfileProvider } from "@/providers/profile-provider/UpdateProfil
 import { GetProfileDataProvider } from "@/providers/profile-provider/getProfileDataProvider";
 import { ChangePasswordProvider } from "@/providers/sign-up-login-provider/changePassowordProvider";
 import { PaymentProvider } from "@/providers/payment-provider/createPaymentProvider";
-<<<<<<< HEAD
 import { GetExploreProvider } from "@/providers/getEcploreProvider";
 import { DonationProvider } from "@/providers/donationProvider";
-=======
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
->>>>>>> b5f21ed (style toast notif)
 
 export default function RootLayout({
   children,
@@ -22,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-<<<<<<< HEAD
         <DonationProvider>
           <AuthenticationProvider>
             <CreateProfileProvider>
@@ -42,24 +36,6 @@ export default function RootLayout({
             </CreateProfileProvider>
           </AuthenticationProvider>
         </DonationProvider>
-=======
-        <AuthenticationProvider>
-          <CreateProfileProvider>
-            <UpdateProfileProvider>
-              <ChangePasswordProvider>
-                <GetProfileDataProvider>
-                  <PaymentProvider>
-                    <CreateAccountProvider>
-                      {children}
-                      <ToastContainer />
-                    </CreateAccountProvider>
-                  </PaymentProvider>
-                </GetProfileDataProvider>
-              </ChangePasswordProvider>
-            </UpdateProfileProvider>
-          </CreateProfileProvider>
-        </AuthenticationProvider>
->>>>>>> b5f21ed (style toast notif)
       </body>
     </html>
   );
